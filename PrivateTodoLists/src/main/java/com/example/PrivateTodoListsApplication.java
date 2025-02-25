523befb930a6a47eaa6fddf126e061ebc56a2b93
masahiro ogata <masahiro ogata@192.168.2.106>
2025-02-25 14:05:26 +0900

    リポジトリ作成

---
+++ PrivateTodoLists/src/main/java/com/example/PrivateTodoListsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateTodoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateTodoListsApplication.class, args);
+	}
+
+}
