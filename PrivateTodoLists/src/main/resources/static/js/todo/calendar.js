e524dbef72ec7084ee52e0bc00908231d314e34d
user <user@192.168.1.98>
2025-03-19 14:38:37 +0900

    カレンダー表示画面修正

--- PrivateTodoLists/src/main/resources/static/js/todo/calendar.js
+++ PrivateTodoLists/src/main/resources/static/js/todo/calendar.js
@@ -2,20 +2,21 @@ window.onload = function() {
 	
 	var todoEvents = [];
 		
-	todoList.forEach((todo) => {
+	todoList.forEach((todo) => {			
 		var event = {};
 		event.id = todo.id;
 		event.title = todo.itemName;
 		event.start = todo.expireDate.substr(0, 10);
+		event.startStr = todo.expireDate.substr(0, 10);
 		if (todo.finishedDate) {
 			event.color = "#0d95f0";
 		} else if (!todo.finishedDate 
-		&& (new Date(todo.expireDate).getTime() < new Date().getTime() - 86400000)) {
+					&& (new Date(todo.expireDate).getTime() < new Date().getTime() - 86400000)) {
 			event.color = "#dc3545";
 		} else {
 			event.color = "#fd7e14"
 		}
-		todoEvents.push(event);
+		todoEvents.push(event);		
 	});
 	
 	var calendarEl = document.getElementById('calendar');
@@ -32,40 +33,65 @@ window.onload = function() {
 	
         events: todoEvents,
         
+        eventClick: function(e) {
+			$("#event-modal").fadeIn(200);
+            $(".modal").fadeIn(200); 
+            $("#event-date").text("");            
+            $("#event-content").html("");
+            $("#no-todo-msg").text('');
+            $(".modal").css("overflow-y", "");     
+            
+            $(".modal").css({'height': '200px','width': '50%'});
+            
+            var clickedEvent = todoList.find((todo) =>{
+				return todo.id == e.event.id;
+			});			
+            var finishedDateMsg;
+            if (clickedEvent.finishedDate != null) {
+				finishedDateMsg = clickedEvent.finishedDate + "完了";
+			} else {
+				finishedDateMsg = "未完了"
+			}
+
+            $("#event-content").append('<tr><td>'
+                         + e.event.title
+                         + '</td><td>'
+                         + e.event.startStr + "まで"
+                         + '</td><td>'
+                         + finishedDateMsg
+                         + '</td></tr>'
+                         ); 
+            
+            $("#todo-transition-btn").text("詳細").removeClass("btn-primary").addClass("btn-outline-success");
+            $("#todo-transition-btn").attr("href","/todo/detail/" + e.event.id);
+            
+			e.jsEvent.stopPropagation(); //クリックイベントの伝播を止める。
+		},
+        
         dateClick: function (info) {
-			console.log(info);
-			var clickedDate = info.dateStr;
+			console.log(info);			
             $("#event-modal").fadeIn(200);
             $(".modal").fadeIn(200);
             
-            $("#event-date").text(clickedDate.substr(0, 4) + "年");
-            if (Number(clickedDate.substr(5, 2)) > 9) {
-				$("#event-date").append(clickedDate.substr(5, 2) + "月");
-			} else {
-				$("#event-date").append(" " + clickedDate.substr(6, 1) + "月");
-			}
-            if(clickedDate.substr(8, 2) > 9) {  
-            	$("#event-date").append(clickedDate.substr(8, 2) + "日");
-            } else {
-				$("#event-date").append(" " + clickedDate.substr(9, 1) + "日");
-			}
+            var clickedDate = info.dateStr.replaceAll("-0", "- ");
+            $("#event-date").text(clickedDate.substr(0, 4) + "年"
+            					+ clickedDate.substr(5, 2) + "月"
+            					+ clickedDate.substr(8, 2) + "日");
             
             $("#event-content").html("");
             $("#no-todo-msg").text('');
             $(".modal").css("overflow-y", "");
             var todaysEvents = todoEvents.filter((event) =>{
-				return event.start == clickedDate
+				return event.start == info.dateStr
 			});
 			console.log(todaysEvents);
 			if (todaysEvents.length == 0) {
 				$(".modal").css({'height': '250px','width': '400px'});
 				$("#no-todo-msg").text('期限が設定された作業はありません');
 			} else {
-				$(".modal").css({'height': '50%','width': '50%'});
+				$(".modal").css({'height': '400px','width': '50%'});
 			}
-			if (todaysEvents.length > 7) {
-				$(".modal").css("overflow-y", "scroll");
-			}
+
 			todaysEvents.forEach((event) => {
 				$("#event-content").append('<tr><td>'
                          + event.title
@@ -75,19 +101,22 @@ window.onload = function() {
                          + '">詳細</a></td></tr>'
                          ); 
  			});
- 			
- 			$("#todo-create-btn").attr("href","/todo/create?expireDate=" + clickedDate);		
+ 			$("#todo-transition-btn").text("作業登録").removeClass("btn-outline-success").addClass("btn-primary");
+ 			$("#todo-transition-btn").attr("href","/todo/create?expireDate=" + info.dateStr);		
 		 }
-		
-		
-        
      });
      
      calendar.render();	
      
-     $(".btn-close").click(function () {
+     $(".btn-close, #modal-close").click(function () {
 		$("#event-modal").hide();
 	 });
+	 
+     $(document).click(function (e) {
+         if (!$(e.target).closest('.modal').length) {
+            $(".modal-wrapper").hide();
+         }
+     });	 
 	 
 	 $(".fc-toolbar-chunk:nth-of-type(3)").prepend(
 		 '<p><span>■</span>完了 <span>■</span>未完了 <span>■</span>期限切れ</p>');
