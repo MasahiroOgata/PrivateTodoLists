276a088221a38dc30ddb904e81bd83ea079aa8a0
MasahiroOgata <ogata0801@gmail.com>
2025-03-29 10:17:40 +0900

    Update schema.sql

--- doc/sql/schema.sql
+++ doc/sql/schema.sql
@@ -3,19 +3,19 @@ CREATE DATABASE IF NOT EXISTS private_todo;
 
 /* ユーザーテーブル作成 */
 CREATE TABLE IF NOT EXISTS private_todo.users (
-    id INT AUTO_INCREMENT NOT NULL,
-    user_id VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    user_name VARCHAR(50) NOT NULL,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+		id INT AUTO_INCREMENT NOT NULL,
+		user_id VARCHAR(50) NOT NULL,
+		password VARCHAR(255) NOT NULL,
+		user_name VARCHAR(50) NOT NULL,
+		create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		PRIMARY KEY (id)
 );
 
 /* TODOテーブル作成 */
-create table if not exists todo_items (
+CREATE TABLE IF NOT EXISTS private_todo.todo_items (
 		id INT AUTO_INCREMENT NOT NULL,
-        user_id INT NOT NULL,
+        	user_id INT NOT NULL,
 		item_name VARCHAR(100) NOT NULL,
 		registration_date DATE,
 		expire_date DATE,
@@ -23,6 +23,17 @@ create table if not exists todo_items (
 		is_deleted TINYINT NOT NULL DEFAULT 0,
 		main_todo INT,
 		create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		PRIMARY KEY (id)
+);
+
+/* 設定テーブル作成 */
+CREATE TABLE IF NOT EXISTS private_todo.settings (
+		id INT AUTO_INCREMENT NOT NULL,
+        	user_id INT NOT NULL,
+            	customize_key VARCHAR(50) NOT NULL,
+            	customize_value VARCHAR(50) NOT NULL,
+            	create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 		update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 		PRIMARY KEY (id)
 );
